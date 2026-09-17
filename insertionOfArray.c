dd3cb1c15517a0641269cd02455fb35f8f31b031
iramfatima99 <aroobmaryam99@gmail.com>
2026-09-17 16:52:49 +0530

    Create insertionOfArray.c

---
+++ insertionOfArray.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, pos, value, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter position: ");
+    scanf("%d", &pos);
+
+    printf("Enter value: ");
+    scanf("%d", &value);
+
+    for (i = n; i >= pos; i--)
+        arr[i] = arr[i - 1];
+
+    arr[pos - 1] = value;
+    n++;
+
+    printf("Array after insertion:\n");
+
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
